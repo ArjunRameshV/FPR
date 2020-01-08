d5c2e26128dadf356504e672f5a7d6007926acdd
ArindamSharma <arindamsharma1998@gmail.com>
2020-01-08 09:37:19 +0530

    Users Friendly message

---
+++ float.py
@@ -0,0 +1,12 @@
+print("Floating point Representation")
+print("Enter chioices :-\n 1.Decimal to Floation Point\n 2.Floating point to Decimal\n 3.Exit")
+while(True):
+	a=int(input("Choice :- "))
+	if(a==1):
+		print("Decimal to Floating point ")
+	elif(a==2):
+		print("Floating point to Decimal")
+	elif(a==3):
+		break
+	else:
+		print("Wrong input")
