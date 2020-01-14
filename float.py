7710b38da203064fcf3a43e5b72ee03d4edfdbd4
ArindamSharma <arindamsharma1998@gmail.com>
2020-01-14 17:52:21 +0530

    Decimal to FPR Done

--- float.py
+++ float.py
@@ -1,3 +1,5 @@
+import os
+os.system("clear")
 # print("Quater Precision Floating point Representation")
 print("Half Precision Floating point Representation")
 # print("Single Precision Floating point Representation")
@@ -8,28 +10,54 @@ while(True):
 		a=int(input("Choice :- "))
 		if(a==1):
 			print("Decimal to Floating point ")
-			num=float(input("Enter a Decimal Number :-"))
+			num=float(input("Enter a Decimal Number : "))
+			sign=str(int(int(num)*-1>0))
 			d=bin(int(num)).split('b')[1]
 			t=int(str(num).split('.')[1])
-			if(len(d)>99):
+			if(len(d)>10):
 				print("too big input for half precision")
 			else:
-				s='';l=len(str(t))
-				for i in range(100):
+				s=''
+				l=len(str(t))
+				for i in range(13):
 					t*=2
 					s+=str(t//(10**l))
 					t=t%(10**l)
-				print(num,d+"."+s)
+				num1=len(d)
+				# print(d+"."+s)
+				d=d+s
+				num2=d.find("1")+1
+				t=num1-num2
+				# print(d[:num2]+"."+d[num2:],t)
+				d=d[d.find("1")+1:]
+				x=bin(15+t).split("b")[1] #Half Precision (max exp of bin size 5)
+				if(len(x)<5):
+					ans=sign+" 0"+x+" "+d[2:12]
+				else:
+					ans=sign+" "+x+" "+d[2:12]
+				print(ans)
+
 		elif(a==2):
 			print("Floating point to Decimal")			
-			num=input("Enter a binary half precision (s e m)format Number :-")
+			num=input("Enter a binary half precision (s e m)format Number : ")
 			# num=num.split(" ")
 			if(len(num)==16):#half precision
 				print("Correct")
-				
+				sign=num[0]
+				exp=num[1:6]
+				man=num[7:]		
+				print(sign,exp,man)
+				exp=int(exp,2)
+				exp=exp-15 #half Precision
+				print(sign,exp,man)
+				ans=''
+				if(sign=='1'):
+					ans="-"
+				temp=int('1'+man[0])
 			else:
 				print("Wrong format")
 		elif(a==3):
+			# os.system("clear")
 			break
 		else:
 			print("Wrong input")
